e7044eb8de2470805836f26c54998d5aa8e2f3a0
Jake Shilling <shilling.jake@gmail.com>
2020-01-09 15:41:43 -0500

    Adding { } around the describe block is a pipe dream :(

--- test/describe-blocks.test.c
+++ test/describe-blocks.test.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <test.h>
 
-it("Has a global test", {
+it("Has a global test",
 
-  });
+   );
 
 describe("First Describe Block",
-    it("Has a test", {
+    it("Has a test",
 
-      });
-	 it("Has another test", {
-	   });
-);
+      );
+	 it("Has another test",
+	   );
+  );
 
 describe("Second Describe Block",
-    it("Has a test", {
-
-      });
-  );
+  it("Has a test",
+                   );
+);
